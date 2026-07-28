0bef1693a43ad026f11f81c02f3f6cb20638ba23
puro-ai <988snv9phn@privaterelay.appleid.com>
2026-07-28 15:52:32 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,1 @@
-const C='wl2-erp-v1-1';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.json']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+self.addEventListener('install',()=>self.skipWaiting());self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.map(k=>caches.delete(k)))).then(()=>self.clients.claim())));self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
