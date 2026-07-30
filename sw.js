283b3fe39f780f6bea6483714c63dc711ad7bb04
puro-ai <988snv9phn@privaterelay.appleid.com>
2026-07-30 18:40:00 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='glowmore-fgms-build-3-3-v1';
+const CACHE='glowmore-fgms-build-3-2-v1';
 const ASSETS=['./','./index.html','./manifest.json'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
